e4389f07f149e68140c2b3e54be880142b40a1a1
AliGunerMD <draliguner@yahoo.com>
2021-04-19 21:30:01 +0300

    peloton - v02

---
+++ slides/Peloton-01-introduction/extras.R
@@ -0,0 +1,55 @@
+title: "Statistical Inference: A Tidy Approach"
+subtitle: The <code><font color="black">infer</font></code> R package
+author: 'Dr. Chester Ismay <br> Senior Curriculum Lead at 
+<a href="https://www.datacamp.com/">DataCamp</a> <br> 
+<a href="http://github.com/ismayc"><i class="fa fa-github fa-fw"></i>&nbsp; ismayc</a><br> 
+<a href="http://twitter.com/old_man_chester"> <i class="fa fa-twitter fa-fw"></i>
+&nbsp; @old_man_chester</a><br> <a href="mailto:chester@datacamp.com">
+<i class="fa fa-paper-plane fa-fw"></i>&nbsp; chester@datacamp.com</a><br>'
+date: 2018-07-12 &ensp; `r knitr::include_graphics("figure/user_au_logo_small.png")` 
+<br><br> Slides available at <http://bit.ly/infer-useR> 
+  <br> Package webpage at <https://infer.netlify.com>
+  
+  
+author: 'Ali Güner  (MD, PhDc, BA, FACS)<br> Professor at 
+<a href="https://www.ktu.edu.tr/">KTU</a> <br> 
+<a href="http://github.com/AliGunerMD"><i class="fa fa-github fa-fw"></i>&nbsp; AliGunerMD</a><br> 
+<a href="http://twitter.com/AliGunerMD"> <i class="fa fa-twitter fa-fw"></i>
+&nbsp; @AliGunerMD</a><br> <a href="mailto:draliguner.yahoo.com">
+<i class="fa fa-paper-plane fa-fw"></i>&nbsp; draliguner@yahoo.com</a><br>'
+  
+  
+  class:title-slide, center, middle
+background-color:white
+
+# `r rmarkdown::metadata$title`
+
+## `r rmarkdown::metadata$subtitle`
+
+<br>
+  
+  ### `r rmarkdown::metadata$author` 
+  ####  <a href= "mailto:draliguner@yahoo.com", style="color:#1278a3">email</a> - <a href="https://aliguner.com/", style="color:#1278a3">web</a> - <a href="https://twitter.com/AliGunerMD", style="color:#1278a3">twitter</a>  - <a href="https://https://github.com/AliGunerMD", style="color:#1278a3">github</a> 
+  <br>
+  #### `r format(Sys.Date(), "%d-%m-%Y")` &#183; KTÜ
+  
+  
+  title: "The Life & Times of a Reproducible Clinical Project in R"
+author: "Jennifer L Thompson, MPH<br>
+<span style = 'font-size: 50%;'>
+Vanderbilt University Medical Center<br>
+Department of Biostatistics + Center for Critical Illness, 
+Brain Dysfunction & Survivorship</span>"
+date: 'R/Medicine 2018<br><br>
+`r icon::fa("link")` [bit.ly/jlt-rmed2018](https://bit.ly/jlt-rmed2018)
+<br>`r icon::fa("twitter")` [jent103](https://twitter.com/jent103)'
+output:
+  xaringan::moon_reader:
+  nature:
+  titleSlideClass: ["left", "middle", "inverse"]
+
+
+
+
+  
+  ---
